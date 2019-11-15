e9d259e649fe1ae8d00e47cf62b6a05841811df0
Ashish <ashishteddy2@gmail.com>
2019-11-15 07:50:45 +0530

    Commit of Sucess!

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:random_color/random_color.dart';
+import 'my_flutter_app_icons.dart';
 
 void main() {
-  runApp( QuotesPage());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title',
+      home: QuotesPage(),
+    );
+  }
 }
 
 class QuotesPage extends StatefulWidget {
@@ -12,28 +24,91 @@ class QuotesPage extends StatefulWidget {
   _QuotesPageState createState() => _QuotesPageState();
 }
 
-Widget drawerRow(String text, IconData icon) {
-  return Row(
+//Widget categories() {
+//  return DropdownButton<String>(
+//    value: 'Categories',
+//    items: <String>['A', 'B', 'C', 'D'].map((String value) {
+//      return new DropdownMenuItem<String>(
+//        value: value,
+//        child: new Text(value),
+//      );
+//    }).toList(),
+//    onChanged: (_) {},
+//  );
+//}
 
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: <Widget>[
-      Text('$text', style: TextStyle(fontSize: 26)),
-      Icon(icon, size: 30,),
-    ],
+Widget drawerRow(String text, IconData icon, Function onPress) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: FlatButton(
+      onPressed: onPress,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text('$text', style: TextStyle(fontSize: 26)),
+          Icon(
+            icon,
+            size: 30,
+          ),
+        ],
+      ),
+    ),
   );
 }
 
-List<Widget> drawerItems = [
-  drawerRow("Categories", Icons.rss_feed),
-  drawerRow("Settings", Icons.settings)
+void showAboutMe(){
+  if(showAbout){
+    showAbout = false;
+  }
+  else{
+    showAbout = true;
+  }
+}
+void changeImage() {
+  drawerImage = mode
+      ? 'https://images.bewakoof.com/t540/stay-awesome-round-neck-3-4th-sleeve-t-shirt-women-s-printed-round-neck-3-4-sleeve-t-shirts-211146-1551690998.jpg'
+      : 'http://edlester.com/wp-content/uploads/2017/01/take-time-to-do-what-makes-your-soul-happy-daily-300x300.jpg';
+}
+
+String drawerImage;
+bool mode = true;
+bool showAbout = false;
+bool isLoading = true;
+bool showCategories = false;
+
+List<Widget> categories = [
+
 ];
 
-bool isLoading = false;
+Widget drawerItems =
+  SingleChildScrollView(
+    scrollDirection: Axis.vertical,
+    child: Column(
+      children: <Widget>[
+        Image.network(drawerImage),
+        //categories(),
+        drawerRow("Categories", Icons.rss_feed, null),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(24, 0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              drawerRow("Home", Icons.home, null),
+              drawerRow("Time", Icons.hourglass_empty, null),
+              drawerRow("Friends", Icons.people_outline, null),
+            ],
+          ),
+        ),
+        drawerRow("Settings", Icons.settings, null),
+        drawerRow("About Us", Icons.tag_faces, showAboutMe),
+      ],
+    ),
+  );
+
+
 
 class _QuotesPageState extends State<QuotesPage> {
   String quote = 'Fetching Secrets to Greatness';
   String author = "You're Awesome";
-  Brightness mode = Brightness.light;
 
   Future<void> getQuotes() async {
     Map jsonMap;
@@ -49,90 +124,133 @@ class _QuotesPageState extends State<QuotesPage> {
     }
   }
 
+  void changeImage() {
+    drawerImage = mode
+        ? 'https://images.bewakoof.com/t540/stay-awesome-round-neck-3-4th-sleeve-t-shirt-women-s-printed-round-neck-3-4-sleeve-t-shirts-211146-1551690998.jpg'
+        : 'http://edlester.com/wp-content/uploads/2017/01/take-time-to-do-what-makes-your-soul-happy-daily-300x300.jpg';
+    setState(() {});
+  }
+
   @override
   void initState() {
+    changeImage();
     getQuotes();
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    var brightness = MediaQuery.of(context).platformBrightness;
-    Color fontColor =
-        brightness == Brightness.light ? Colors.white : Colors.white;
+    void _showcontent() {
+      showDialog<Null>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: new CircleAvatar(
+              radius: 64,
+              child: Image.network('https://cdn.nohat.cc/thumb/f/720/4768618252337152.jpg'),
+            ),
+            content: new SingleChildScrollView(
+              child: new ListBody(
+                children: <Widget>[
+                  new Text('About Me'),
+                  Text('r/madlads')
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+    Color fontColor = mode ? Colors.white : Colors.white;
     RandomColor _randomColor = RandomColor();
     var screenSize = MediaQuery.of(context).size.width;
     Color randColor = _randomColor.randomColor();
-    return MaterialApp(
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: mode,
-      ),
-      home : Scaffold(
-
-          drawer: Drawer(
-            child: new Column(
-              children: <Widget>[
-                new UserAccountsDrawerHeader(
-                    accountName: new Text("John Doe"), accountEmail: null),
-                new Column(children: drawerItems)
-              ],
-            ),
+    return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: mode ? Color(0xffe3c7e0) : Color(0xff222732),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Image.network(drawerImage),
+              //categories(),
+              drawerRow("Categories", Icons.rss_feed, null),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 0, 0, 0),
+                child: Column(
+                  children: <Widget>[
+                    drawerRow("Home", Icons.home, null),
+                    drawerRow("Time", Icons.hourglass_empty, null),
+                    drawerRow("Friends", Icons.people_outline, null),
+                  ],
+                ),
+              ),
+              drawerRow("Settings", Icons.settings, null),
+              drawerRow("About Us", Icons.tag_faces, _showcontent),
+            ],
           ),
-          body: Center(
-            child: Container(
+        ),
+      ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+
+            AnimatedContainer(
+              duration: Duration(milliseconds: 500),
               decoration: BoxDecoration(
-                  // Box decoration takes a gradient
-                  gradient: brightness == Brightness.light
+                // Box decoration takes a gradient
+                  gradient: mode
                       ? LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          stops: [
-                              0.1,
-                              0.4,
-                              0.6,
-                              0.9
-                            ],
-                          colors: [
-                              Colors.yellow,
-                              Colors.red,
-                              Colors.indigo,
-                              Colors.teal
-                            ])
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Colors.blue, Colors.red])
                       : LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [Colors.indigo, Colors.black87])),
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Colors.indigo, Colors.black87])),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Container(
                     child: Center(
-                      child: IconButton(
-                        iconSize: 46,
-                        icon: mode == Brightness.light
-                            ? Icon(Icons.brightness_5, color: Colors.yellow,)
-                            : Icon(Icons.brightness_3, color: Colors.white,),
-                        onPressed: (){
-                          if (MediaQuery.of(context).platformBrightness == Brightness.light){
-                            setState(() {
-                              mode = Brightness.dark;
-                            });
-                          }
-                          else{
-
-                            setState(() {
-                              mode = Brightness.light;
-
-                            });
-                          }
-                        },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
+                        child: IconButton(
+                          iconSize: 46,
+                          icon: mode
+                              ? Icon(
+                            Icons.brightness_7,
+                            color: Colors.yellow,
+                          )
+                              : Icon(
+                            Icons.brightness_3,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            if (mode == true) {
+                              setState(() {
+                                mode = false;
+                              });
+                            } else {
+                              setState(() {
+                                mode = true;
+                              });
+                            }
+                          },
+                        ),
                       ),
                     ),
                   ),
-
                   Container(
                     height: screenSize * 0.8,
                     width: double.infinity,
@@ -145,86 +263,86 @@ class _QuotesPageState extends State<QuotesPage> {
                               vertical: 24, horizontal: 32),
                           child: isLoading
                               ? Text(
-                                  'Loading Secrets for Success.',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      color: fontColor),
-                                  textAlign: TextAlign.left,
-                                )
+                            'Loading Secrets for Success.',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: fontColor),
+                            textAlign: TextAlign.left,
+                          )
                               : Text(
-                                  '$quote',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      color: fontColor),
-                                  textAlign: TextAlign.left,
-                                ),
+                            '$quote',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: fontColor),
+                            textAlign: TextAlign.left,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(
                               vertical: 4, horizontal: 32),
                           child: isLoading
                               ? Text(
-                                  'You are Amazing!!',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontStyle: FontStyle.italic,
-                                    color: fontColor,
-                                  ),
-                                  textAlign: TextAlign.left,
-                                )
+                            'You are Amazing!!',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                              color: fontColor,
+                            ),
+                            textAlign: TextAlign.left,
+                          )
                               : Text(
-                                  '$author',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontStyle: FontStyle.italic,
-                                    color: fontColor,
-                                  ),
-                                  maxLines: 1,
-                                  textAlign: TextAlign.left,
-                                ),
+                            '$author',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                              color: fontColor,
+                            ),
+                            maxLines: 1,
+                            textAlign: TextAlign.left,
+                          ),
                         ),
                       ],
                     ),
                   ),
                   Padding(
                     padding:
-                        const EdgeInsets.symmetric(vertical: 48, horizontal: 32),
+                    const EdgeInsets.symmetric(vertical: 48, horizontal: 32),
                     child: ActionGradientButton(
                       child: isLoading
                           ? CircularProgressIndicator(
-                              strokeWidth: 3.0,
-                              backgroundColor: Colors.white,
-                              valueColor: AlwaysStoppedAnimation<Color>(
-                                randColor,
-                              ),
-                              value: null,
-                            )
+                        strokeWidth: 3.0,
+                        backgroundColor: Colors.white,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          randColor,
+                        ),
+                        value: null,
+                      )
                           : Icon(
-                              Icons.arrow_forward,
-                              color: Colors.white,
-                              size: 21,
-                            ),
-                      gradient: brightness == Brightness.dark
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                        size: 21,
+                      ),
+                      gradient: mode
                           ? LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              stops: [0.45, 0.90],
-                              colors: [Colors.blueAccent, Colors.greenAccent])
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          stops: [0.45, 0.90],
+                          colors: [Colors.blueAccent, Colors.greenAccent])
                           : LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              stops: [
-                                  0.0,
-                                  0.35,
-                                  0.65
-                                ],
-                              colors: [
-                                  Colors.yellow,
-                                  Colors.orange,
-                                  Colors.redAccent
-                                ]),
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          stops: [
+                            0.0,
+                            0.35,
+                            0.65
+                          ],
+                          colors: [
+                            Colors.yellow,
+                            Colors.orange,
+                            Colors.redAccent
+                          ]),
                       onPressed: () {
                         setState(() {
                           randColor = _randomColor.randomColor();
@@ -237,7 +355,32 @@ class _QuotesPageState extends State<QuotesPage> {
                 ],
               ),
             ),
-          )),
+            Center(
+              child: Visibility(
+                visible: showAbout,
+                child : AlertDialog(
+                  title: new Text('You clicked on'),
+                  content: new SingleChildScrollView(
+                    child: new ListBody(
+                      children: <Widget>[
+                        new Text('This is a Dialog Box. Click OK to Close.'),
+                      ],
+                    ),
+                  ),
+                  actions: <Widget>[
+                    new FlatButton(
+                      child: new Text('Ok'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -260,7 +403,8 @@ class ActionGradientButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
       width: 60,
       height: 60.0,
       decoration: BoxDecoration(
@@ -284,4 +428,3 @@ class ActionGradientButton extends StatelessWidget {
     );
   }
 }
-
